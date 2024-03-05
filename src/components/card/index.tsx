c5175fc6cd7605b29ab74f059896ae1ccf2410ad
Hossam Ali <hossamm.ali1993@gmail.com>
2024-03-05 18:53:48 +0100

    adjust colors

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -19,7 +19,7 @@ const ChatCard: FC<ChatCardProps> = ({
 }) => (
   <Box className={`card-component ${direction}`}>
     <Avatar
-      sx={{ bgcolor: direction === 'left' ? lightBlue[500] : '#4caf50' }}
+      sx={{ bgcolor: direction === 'left' ? lightBlue[500] : '#61c77a' }}
       className="avatar-img"
     >
       N
